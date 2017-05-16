90c512b07c234aa2a74d7df8276add3e31afafe9
Bt <blacktoque@shaw.ca>
2017-05-16 16:28:57 -0600

    update with new survy json file, starting dev branch

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -17,20 +17,21 @@ class AboutSection extends Component {
                         <br /><button className="btn btn-default btn-lg">Get in Touch</button>
                     </div>
                     <div id="intromsg" className="w3-panel w3-josefin">
-                        <p>
-                            This site serves to showcase examples of various techniques used in the production of web applications. There are
+                       <p>
+                           This site serves to showcase examples of various techniques used in the production of web applications. There are
                                    many different technologies used today, each depending on the expectations and needs of the client base.
                        </p>
-                        <p>
+                       <p>
                            This is an example of a Multi Page Application. Most sections (About, Services, etc) are created as
                            <a style={sty} href="https://github.com/facebook/react"> React components</a>.  Rather than the main App composing the final render,
-                           as in an SPA, this is using React's <a style={sty}  href="https://github.com/ReactTraining/react-router"> Router</a> to render individual components 
-                               on a page. 
-                           <p className="well">For an example of an Single Page Application using this template see <a href="https://ex0001.herokuapp.com/"> BlackToque - SPA </a></p>
+                           as in an SPA, this is using React's <a style={sty} href="https://github.com/ReactTraining/react-router"> Router</a> to render individual components
+                           on a page. </p>
+                       <p className="well">For an example of an Single Page Application using this template see <a href="https://ex0001.herokuapp.com/"> BlackToque - SPA </a></p>
+                       <p>
                            The theme is based on a <a style={sty} href="https://www.w3schools.com/bootstrap/bootstrap_theme_company.asp" target="_blank"> Bootstrap template for a company.</a>
-                                The Application Framework is <a style={sty} href="https://expressjs.com/" target="_blank"> Express 4.15.2</a> for
+                           The Application Framework is <a style={sty} href="https://expressjs.com/" target="_blank"> Express 4.15.2</a> for
                                    <a style={sty} href="https://nodejs.org/en/" target='_blank'> Node.js</a>
-                        </p>
+                       </p>
 
                     </div>
                     <div className="col-sm-4">
